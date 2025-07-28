d111bff3a06f0c9c3906f1e26fd619e9072011cf
Ion Erhan <ionerhan13@gmail.com>
2025-07-28 21:25:16 +0300

    Fetch exercises

--- script.js
+++ script.js
@@ -21,16 +21,40 @@
 // ------------------------Add/delete list elements in --------------------
 
 
-async function getData() {
-    let url = 'https://jsonplaceholder.typicode.com/users';
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error('Response status'.response.status)
-        }
-        const json = await response.json();
-    } catch (error) {
-        console.log(error.message)
-    }
-}
+// async function getData() {
+//     let url = 'https://jsonplaceholder.typicode.com/users';
+//     try {
+//         const response = await fetch(url);
+//         if (!response.ok) {
+//             throw new Error('Response status'.response.status)
+//         }
+//         const json = await response.json();
+//     } catch (error) {
+//         console.log(error.message)
+//     }
+// }
 
+// import fetch from 'node-fetch';
+// import fs from 'fs/promises';
+
+// const response = fetch('https://jsonplaceholder.typicode.com/posts/1');
+// response.then(result => result.json())
+//     .then(resuult => console.log(resuult))
+//         .catch(error => console.log(error))
+
+
+(async function () {
+    const url = "https://w.wallhaven.cc/full/5y/wallhaven-5yd6d5.png";
+    try {
+        const initialResponse = await fetch(url);
+        if (!initialResponse.ok) {
+            throw new Error('Response status'. initialResponse.status)
+        }
+        const blobq = await initialResponse.blob();
+        const objectURL = await URL.createObjectURL(blobq);
+        
+        
+    } catch (error) {
+        console.log(error);
+    }
+})();
