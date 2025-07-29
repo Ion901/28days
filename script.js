a6aa96851f772120dc847841a1528d1e9fcf873d
Ion Erhan <ionerhan13@gmail.com>
2025-07-29 15:45:00 +0300

    fetchData and searchTitle feature

--- script.js
+++ script.js
@@ -43,18 +43,78 @@
 //         .catch(error => console.log(error))
 
 
-(async function () {
-    const url = "https://w.wallhaven.cc/full/5y/wallhaven-5yd6d5.png";
+// (async function () {
+//     const url = "https://w.wallhaven.cc/full/5y/wallhaven-5yd6d5.png";
+//     try {
+//         const initialResponse = await fetch(url);
+//         if (!initialResponse.ok) {
+//             throw new Error('Response status'. initialResponse.status)
+//         }
+//         const blobq = await initialResponse.blob();
+//         const objectURL = await URL.createObjectURL(blobq);
+        
+        
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })();
+
+const document1 = document.querySelector('#day13');
+
+ async function fetchData() {
+    const url = "https://jsonplaceholder.typicode.com/posts";
     try {
-        const initialResponse = await fetch(url);
-        if (!initialResponse.ok) {
-            throw new Error('Response status'. initialResponse.status)
-        }
-        const blobq = await initialResponse.blob();
-        const objectURL = await URL.createObjectURL(blobq);
+        const fragment = document.createDocumentFragment();
+        const list = document.createElement('ul');
+
+        const response = await fetch(url);
+        const result = await response.json();
+
+            result.map(obj => {
+                const elements = document.createElement('li');
+                elements.textContent = `${obj.title}`
+                fragment.appendChild(elements);
+            })
+            list.appendChild(fragment);
+
+        document1.appendChild(list);
         
-        
+        titleSearch();
     } catch (error) {
         console.log(error);
     }
-})();
+}
+
+
+//Logica functionarii:
+//Selectam toate titlurile, transformam search-term in litere mici si fara spatii, transformam titlurile in litere mici
+//iteram prin toate titlurile si verificam daca acestea includ in ele (.includes()) search-term nostru
+//la fiecare apasare pe tastatura prin keyup, verificam acest lucru
+function titleSearch() {
+    
+    const search = document.querySelector('#title-search');
+    
+    let titles = document1.getElementsByTagName('li');
+    let iterablesTitles = [...titles];
+    console.log(iterablesTitles);
+    
+    search.addEventListener('keyup', function () {
+    const query = search.value.trim().toLowerCase();//scoate spatiile libere si le face pe toate litere mici
+    
+        iterablesTitles.forEach(title => {
+            const title1 = title.innerText.toLowerCase();// le face pe toate litere mici 
+            
+            if (title1.includes(query)) {
+                title.style.backgroundColor = "red";
+            } else {
+                title.style.backgroundColor = "transparent"
+            }
+        })
+    })
+}
+fetchData();
+
+
+
+
+
